a9a9717cd566741f1984faec17900df02dc0f172
GhostyAD-TranThanhTrung <tranthanhtrung2015@gmail.com>
2026-03-19 23:53:24 +0700

    Add image upload middleware to teacher image routes for enhanced file handling

--- swd392backEND/src/route/ClassRoute.ts
+++ swd392backEND/src/route/ClassRoute.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import ClassController from "../controller/ClassController.ts";
 import verifyRole from "../ultis/verifyRole.ts";
+import { imageMulterUpload } from "../ultis/cloudinary.ts";
 const route = Router();
 
 // Admin statistics for classes
@@ -16,8 +17,8 @@ route.get("/class/:classId/students", verifyRole.verifyTeacher, ClassController.
 route.post("/class", verifyRole.verifyTeacher, ClassController.createClass);
 route.put("/class/:id", verifyRole.verifyTeacher, ClassController.updateClass);
 route.post("/class/:classId/generate-keypass", verifyRole.verifyTeacher, ClassController.generateKeypass);
-route.post("/teacher/upload-image", verifyRole.verifyTeacher, ClassController.uploadImageCover);
-route.put("/teacher/update-image", verifyRole.verifyTeacher, ClassController.updateImageCover);
+route.post("/teacher/upload-image", verifyRole.verifyTeacher, imageMulterUpload.single("image"), ClassController.uploadImageCover);
+route.put("/teacher/update-image", verifyRole.verifyTeacher, imageMulterUpload.single("image"), ClassController.updateImageCover);
 route.delete("/teacher/delete-image", verifyRole.verifyTeacher, ClassController.deleteImageCover);
 
 // Teacher delete class (with ownership check)
