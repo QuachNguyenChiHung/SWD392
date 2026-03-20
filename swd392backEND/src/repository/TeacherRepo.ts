bb947e61701eb5e990bd18a2b93eb7b2f59f45ca
QuachNguyenChiHung <bacon3632@gmail.com>
2026-03-20 11:15:44 +0700

    Add package-lock.json and enhance TeacherRepo and TeacherService for improved data retrieval and documentation

--- swd392backEND/src/repository/TeacherRepo.ts
+++ swd392backEND/src/repository/TeacherRepo.ts
@@ -2,9 +2,11 @@ import { Teacher } from "../entities/Teacher.ts";
 
 class TeacherRepo {
     async getTeacherById(teacherId: string) {
-        return await Teacher.findById(teacherId);
+        return await Teacher.findById(teacherId).populate("user_id", "-password");
     }
-
+    async getTeacherByUserId(userId: string) {
+        return await Teacher.findOne({ user_id: userId }).populate("user_id", "-password");
+    }
     async createTeacher(userId: string, credentialLink: string, fileName: string) {
         const teacher = new Teacher({
             user_id: userId,
